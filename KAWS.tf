e185cd1ab4255bf828f9ababa3f5e79d3dca28c6
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-10-25 11:44:59 +0530

    Add files via upload

---
+++ KAWS.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_cluster" "test" {
+  name     = "example"
+  encryption_config {
+    resources = ["secrets"]
+  }
+  labels = {
+      test = ["MyExampleApp"]
+    }
+  tags = {
+      ENV = "Poduction"
+  }
+}
